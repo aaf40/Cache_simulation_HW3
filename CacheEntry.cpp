d262082061cd06a659c2bd049acbf6c48fce082a
aaf40 <aaf40@txstate.edu>
2024-04-19 16:32:09 -0500

    Changed tag to an integer in CacheEntry.cpp

--- CacheEntry.cpp
+++ CacheEntry.cpp
@@ -1,8 +1,8 @@
 class CacheEntry {
 public:
     bool valid;
-    int* tag;  // Using pointer to int for tag to easily represent null (invalid)
+    int tag;  // Using pointer to int for tag to easily represent null (invalid)
 
     // Constructor to initialize cache entry
-    CacheEntry() : valid(false), tag(nullptr) { }
+    CacheEntry() : valid(false), tag(-1) { }
 };
