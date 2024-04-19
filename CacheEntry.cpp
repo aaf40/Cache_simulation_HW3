520201066fe8d36fcaf20a0890db0e920d292cc8
aaf40 <aaf40@txstate.edu>
2024-04-19 16:51:17 -0500

    removing comments

--- CacheEntry.cpp
+++ CacheEntry.cpp
@@ -1,8 +1,7 @@
 class CacheEntry {
 public:
     bool valid;
-    int tag;  // Using pointer to int for tag to easily represent null (invalid)
+    int tag; 
 
-    // Constructor to initialize cache entry
     CacheEntry() : valid(false), tag(-1) { }
 };
